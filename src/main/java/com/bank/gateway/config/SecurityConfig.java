fe033630cd1dc40471373cb9188a47a028ea7328
Fernando Cruz Cavina <fernando.cruz.cavina@gmail.com>
2025-05-18 20:37:49 -0300

    initial commit

---
+++ src/main/java/com/bank/gateway/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.bank.gateway.config;
+
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+            .authorizeExchange(auth -> auth
+                .pathMatchers("/auth/**").permitAll()
+                .pathMatchers("/actuator/**").permitAll()
+                .anyExchange().authenticated())
+            .csrf(csrf -> csrf.disable())
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                    .jwtDecoder(jwtDecoder())
+                )
+            );
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withPublicKey(publicKey).build();
+    }
+}
