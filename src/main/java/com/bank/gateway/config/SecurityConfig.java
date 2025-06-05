ea0c2474fa5a58e79357e9a05ccb04ae5e194df7
FernandoCruzCavina <fernando.cruz.cavina@gmail.com>
2025-06-04 22:01:11 -0300

    fix: 🛠️ fix fallback url

--- src/main/java/com/bank/gateway/config/SecurityConfig.java
+++ src/main/java/com/bank/gateway/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bank.gateway.config;
 
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,11 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,6 +29,7 @@ public class SecurityConfig {
         http
                 .authorizeExchange(auth -> auth
                         .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/user/**").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/account/**").permitAll()
                         .pathMatchers("/payment/**").permitAll()
@@ -38,5 +45,17 @@ public class SecurityConfig {
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
         return NimbusReactiveJwtDecoder.withPublicKey(publicKey).build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173")); 
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
